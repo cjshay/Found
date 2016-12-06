56db80d3f0d54dcfdfa29903ca0cb5024930bfef
cjshay <christopher.jackson.shay@gmail.com>
2016-12-06 14:38:30 -0500

    Npm install

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  context: __dirname,
+  entry: './frontend/medium.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
+//
