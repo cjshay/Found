b61dafa45752ebbfcb7e9886987ba91936b776a1
cjshay <christopher.jackson.shay@gmail.com>
2016-12-16 12:27:07 -0500

    work on migration issue

--- db/migrate/20161215200435_add_attachment_to_story.rb
+++ db/migrate/20161215200435_add_attachment_to_story.rb
@@ -1,5 +1,13 @@
 class AddAttachmentToStory < ActiveRecord::Migration[5.0]
-  
+  def self.up
+    change_table :stories do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :stories, :image
+  end
   def self.up
     change_table :users do |t|
       t.attachment :image
--- db/migrate/20161216160435_add_image_to_stories.rb
+++ db/migrate/20161216160435_add_image_to_stories.rb
@@ -1,11 +1,3 @@
 class AddImageToStories < ActiveRecord::Migration[5.0]
-  def self.up
-    change_table :stories do |t|
-      t.attachment :image
-    end
-  end
-
-  def self.down
-    remove_attachment :stories, :image
-  end
+  
 end
