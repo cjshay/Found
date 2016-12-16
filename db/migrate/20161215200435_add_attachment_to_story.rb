8d559be43cacf7fed9647cf5f90352104cc9b681
cjshay <christopher.jackson.shay@gmail.com>
2016-12-16 12:24:34 -0500

    Fix migration files

--- db/migrate/20161215200435_add_attachment_to_story.rb
+++ db/migrate/20161215200435_add_attachment_to_story.rb
@@ -1,13 +1,5 @@
 class AddAttachmentToStory < ActiveRecord::Migration[5.0]
-  def self.up
-    change_table :stories do |t|
-      t.attachment :image
-    end
-  end
-
-  def self.down
-    remove_attachment :stories, :image
-  end
+  
   def self.up
     change_table :users do |t|
       t.attachment :image
